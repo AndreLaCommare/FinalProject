5a72dc201c75f8a55bd3fd2d64f260e6a74c7a0e
tommcglone1 <tom.mcglone1@yahoo.com>
2023-04-18 15:24:55 -0400

    Build User entity, pass JUINT test

--- JPAMeals/src/test/java/com/skilldistiller/meals/entities/UserTest.java
+++ JPAMeals/src/test/java/com/skilldistiller/meals/entities/UserTest.java
@@ -1,6 +1,12 @@
 package com.skilldistiller.meals.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -8,27 +14,41 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.skilldistillery.meals.entities.User;
+
 class UserTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMeals");
+		
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em =emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user=null;
 	}
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
 	}
 
 }
