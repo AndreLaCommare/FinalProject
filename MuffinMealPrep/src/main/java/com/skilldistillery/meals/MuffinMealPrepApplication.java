da24754b608e9d3f3a07132c1e8d7c7a1de9a08b
andre_lacommare <alacommaresb@gmail.com>
2023-04-18 10:07:33 -0700

    Spring boot Project created

---
+++ MuffinMealPrep/src/main/java/com/skilldistillery/meals/MuffinMealPrepApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.meals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuffinMealPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuffinMealPrepApplication.class, args);
+	}
+
+}
---
+++ MuffinMealPrep/src/test/java/com/skilldistillery/meals/MuffinMealPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.meals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuffinMealPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
