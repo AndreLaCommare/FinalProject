a329d85775cc14efcd43f826939820a25835955e
andre_lacommare <alacommaresb@gmail.com>
2023-04-18 10:21:13 -0700

    Configure Spring Security

--- MuffinMealPrep/src/main/java/com/skilldistillery/meals/MuffinMealPrepApplication.java
+++ MuffinMealPrep/src/main/java/com/skilldistillery/meals/MuffinMealPrepApplication.java
@@ -2,12 +2,20 @@ package com.skilldistillery.meals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MuffinMealPrepApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MuffinMealPrepApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
 	}
 
 }
