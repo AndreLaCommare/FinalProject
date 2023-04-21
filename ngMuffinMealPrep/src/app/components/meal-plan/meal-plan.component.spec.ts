cea562d45ad2fb6bae7ee15ac7940927556e6e5a
andre_lacommare <alacommaresb@gmail.com>
2023-04-21 12:47:17 -0700

    app module local error fixed

---
+++ ngMuffinMealPrep/src/app/components/meal-plan/meal-plan.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MealPlanComponent } from './meal-plan.component';
+
+describe('MealPlanComponent', () => {
+  let component: MealPlanComponent;
+  let fixture: ComponentFixture<MealPlanComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MealPlanComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MealPlanComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
