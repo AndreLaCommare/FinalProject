e2d813ecbeafb89c6395b0a04f47e922848fbe5d
JKantor <boomersooner918@gmail.com>
2023-04-23 14:59:05 -0600

    Model done and service methods created

--- ngMuffinMealPrep/src/app/models/meal-review.ts
+++ ngMuffinMealPrep/src/app/models/meal-review.ts
@@ -1,2 +1,31 @@
+// src/app/models/meal-review.model.ts
 export class MealReview {
+  id: number;
+  user: number;
+  meal: number;
+  comment: string;
+  stars: number;
+  enabled: boolean;
+  createdAt: string;
+  updatedAt: string;
+
+  constructor(
+    id: number = 0,
+    user: number = 0,
+    meal: number = 0,
+    comment: string = '',
+    stars: number = 0,
+    enabled: boolean = false,
+    createdAt: string = '',
+    updatedAt: string = ''
+  ) {
+    this.id = id;
+    this.user = user;
+    this.meal = meal;
+    this.comment = comment;
+    this.stars = stars;
+    this.enabled = enabled;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
 }
