6a9352d78315348e1dc57a3ea6d5a4ca5f3ec5d9
Thomas McGlone <tom@Toms-MacBook-Pro.local>
2023-04-24 10:51:33 -0400

    Shopping List and Mealreview Fixed

--- ngMuffinMealPrep/src/app/models/meal-review.ts
+++ ngMuffinMealPrep/src/app/models/meal-review.ts
@@ -1,8 +1,10 @@
+import { Meal } from "./meal";
+
 // src/app/models/meal-review.model.ts
 export class MealReview {
-  id: number;
-  user: number;
-  meal: number;
+  // id: number;
+  // user: number;
+  meal: Meal;
   comment: string;
   stars: number;
   enabled: boolean;
@@ -10,17 +12,17 @@ export class MealReview {
   updatedAt: string;
 
   constructor(
-    id: number = 0,
-    user: number = 0,
-    meal: number = 0,
+    // id: number = 0,
+    // user: number = 0,
+    meal: Meal = new Meal(),
     comment: string = '',
     stars: number = 0,
     enabled: boolean = false,
     createdAt: string = '',
     updatedAt: string = ''
   ) {
-    this.id = id;
-    this.user = user;
+    // this.id = id;
+    // this.user = user;
     this.meal = meal;
     this.comment = comment;
     this.stars = stars;
